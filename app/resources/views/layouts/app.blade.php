83456820b725ffe309902fc82476480413d0eeea
“Ricky <grickyh@gmail.com>
2022-04-27 09:14:11 +0700

    Update Views

--- app/resources/views/layouts/app.blade.php
+++ app/resources/views/layouts/app.blade.php
@@ -23,8 +23,9 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">
+                    <div><img src="logo.png" style="height: 20px; border-right: 1.5px solid #ffff;" class="pr-7"></div>
+                    <div class="pl-7">{{ config('app.name', 'Laravel') }}</div>
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
